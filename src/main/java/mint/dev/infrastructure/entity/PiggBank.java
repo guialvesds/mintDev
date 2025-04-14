7e61b8b74238692aed5a767c1b6d55cb7a41920f
guialvesds <guilherme_ealves@outlook.com>
2025-04-14 16:29:46 -0300

    Implemented entity for Pigg Bank

---
+++ src/main/java/mint/dev/infrastructure/entity/PiggBank.java
@@ -0,0 +1,27 @@
+package mint.dev.infrastructure.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document("piggbank")
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class PiggBank {
+
+    @Id
+    private String id;
+    private Double balance;
+    private String name;
+    private Double goal;
+    private Double goalPerc;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private UserEntity user;
+}
